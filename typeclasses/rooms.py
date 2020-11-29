ce34e92043f2b941366c9ab1d54cf395518d28d7
lcanady <lem@digibear.io>
2020-11-28 21:58:42 -0800

    Lifepath work, room parent desc work

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -4,8 +4,14 @@ Room
 Rooms are simple containers that has no location of their own.
 
 """
+from collections import defaultdict
 
 from evennia import DefaultRoom
+from evennia.utils import wrap, time_format
+
+
+def format_time(idle_time, param):
+    pass
 
 
 class Room(DefaultRoom):
@@ -19,4 +25,68 @@ class Room(DefaultRoom):
     properties and methods available on all Objects.
     """
 
-    pass
+    def return_appearance(self, looker, **kwargs):
+        """
+        This formats a description. It is the hook a 'look' command
+        should call.
+
+        Args:
+            looker (Object): Object doing the looking.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call (unused by default).
+        """
+
+        def char_string(char):
+            """
+            Format the display from the character object to show some information about each character in the room
+            when one does a look command.
+            """
+            output = ""
+            if con.is_superuser:
+                output += " |h|y*|n "
+            else:
+                output += "   "
+            output += con.get_display_name(char).ljust(20)
+            if con == char:
+                output += "0s".rjust(5)
+            else:
+                output += time_format(con.idle_time, 1).rjust(5)
+            output += "  "
+            if con.db.shotdesc:
+                output += (con.db.shortdesc[:45] + "...") if len(con.db.shortdesc) > 48 else con.db.shortdesc
+            else:
+                output += "|h|xUse '+shortdesc <desc>' to set this field.|n"
+            output += "\n"
+            return output
+
+        if not looker:
+            return ""
+        # get and identify all objects
+        visible = (con for con in self.contents if con.access(looker, "view"))
+        exits, users, things = [], [], defaultdict(list)
+        for con in visible:
+            key = con.get_display_name(looker)
+            if con.destination:
+                exits.append(con)
+            elif con.has_account:
+                users.append(char_string(con))
+            else:
+                # things can be pluralized
+                things[key].append(con)
+        # get description, build string
+        string = f"--->> |h{self.get_display_name(looker)}|n >>".ljust(82, "-") + "\n\n"
+        desc = wrap(self.db.desc, width=78)
+        if desc:
+            string += "%s\n\n" % desc
+        string += "--->> |hCharacters|n >>".ljust(82, "-") + "\n"
+        string += "|hName|n".ljust(28) + "|hIdle|n".rjust(5) + "  " + "|hShort Description|n\n"
+        for user in users:
+            string += user
+        string += "\n"
+        if exits:
+            string += "--->> |hExits|n >>".ljust(82, "-") + "\n"
+            for ext in exits:
+                aliases = ext.aliases.all()
+                aliases.sort()
+                string += f"|h{ext.get_display_name(looker)}|n-<|h{aliases[0].upper()}|n>\n"
+        return string
--- world/data/lifepath.py
+++ world/data/lifepath.py
@@ -1,292 +1,273 @@
-import random
-
 # Cultural Origins
+BASIC_LIFEPATH = {
+    "cultural Origins": [
+        {
+            "name": "North American",
+            "lang": (
+                "Chineese",
+                "Cree",
+                "Creole",
+                "English",
+                "French",
+                "Navajo",
+                "Spanish"
+            )
+        },
+        {
+            "name": "South/Central American",
+            "lang": (
+                "Creole",
+                "English",
+                "German",
+                "Guarani",
+                "Mayan",
+                "Portugese",
+                "Quechua",
+                "Spanish"
+            )
+        },
+        {
+            "Name": "Western European",
+            "langs": (
+                "Dutcch",
+                "English",
+                "French",
+                "German",
+                "Italian",
+                "Norwegian",
+                "Portuguese",
+                "spanish"
+            )
+        },
+        {
+            "name": "Eastern European",
+            "langs": (
+                "English",
+                "Finnish",
+                "Polish",
+                "Romanian",
+                "Russian",
+                "Ukranian"
+            )
+        },
+        {
+            "name": "Middle Eastern/North African",
+            "langs": (
+                "Arabic",
+                "Burber",
+                "English",
+                "Farsi",
+                "French",
+                "Hebrew",
+                "Turkish"
+            )
+        },
+        {
+            "name": "Sub-Saharan African",
+            "langs": (
+                "Arabic",
+                "English",
+                "French",
+                "Hausa",
+                "Lingala",
+                "Oromo",
+                "Portugese",
+                "Swahihli",
+                "Twi",
+                "Yoruba"
+            )
+        },
+        {
+            "name": "South Asian",
+            "langs": (
+                "Bengali",
+                "Dari",
+                "English",
+                "Hindi",
+                "Nepali",
+                "Sinhalese",
+                "Tamil",
+                "Urdu"
+            )
+        },
+        {
+            "name": "South East Asian",
+            "langs": (
+                "Arabic",
+                "English",
+                "Filipeno",
+                "Hindi",
+                "Indonesian",
+                "Khmer",
+                "Malayan",
+                "Vietnamese"
+            )
+        },
+        {
+            "name": "East Asian",
+            "langs": (
+                "Cantonese",
+                "Chinese",
+                "English",
+                "Japanese",
+                "Korean",
+                "Mandarin Chinese",
+                "Mongolian"
+            )
+        },
+        {
+            "name": "Oceana/Pacific Islander",
+            "langs": (
+                "English",
+                "French",
+                "Hawaiian",
+                "Maori",
+                "Pama-Nyungan",
+                "Tahitan"
+            )
+        }
+    ],
+    "What are you like?":
+        (
+            "Shy and secretive",
+            "Rebellious, antisocialm and violent",
+            "Arrogant, proud and aloof",
+            "Moody, rash and headstrong",
+            "Picky, fussy, nervous",
+            "Stable and serious",
+            "Silly and fluff-headed",
+            "Stable and serious",
+            "Sneaky and deceptive",
+            "intellectual and detached",
+            "Friendly and outgoing"
+        ),
+    "What's your preferred clothing style?": (
+        "Generic Chic (Standard, Colorful, Modular)",
+        "Leisurewear (Comfort, Agility, Athleticism",
+        "Urban Flash (Flashy, Technological, Streetwear)",
+        "Businesswear (Leadership, Presence, Authority)",
+        "High Fashion (Exclusive, Designer, Couture)",
+        "Bohemian (Folksy, Retro, Free-Spirited)",
+        "Bag Lady Chic (Homeless, Ragged, Vagrant)",
+        "Gang Colors (Dangerous, Violent, Rebelious)",
+        "Nomad Leathers (Western, Rugged, Tribal)",
+        "Asia Pop (Bright, Costume-like, Youthful)"
+    ),
+    "What kind of hair style do you prefer?": (
+        "Mohawk",
+        "Long and ratty",
+        "Short and spiked",
+        "Wild and all over",
+        "Bald",
+        "Striped",
+        "Wild colors",
+        "Neat and short",
+        "Short and curly",
+        "Long and straight"
+    ),
 
-CULTURE = [
-    {
-        "name": "North American",
-        "lang": (
-            "Chineese",
-            "Cree",
-            "Creole",
-            "English",
-            "French",
-            "Navajo",
-            "Spanish"
-        )
-    },
-    {
-        "name": "South/Central American",
-        "lang": (
-            "Creole",
-            "English",
-            "German",
-            "Guarani",
-            "Mayan",
-            "Portugese",
-            "Quechua",
-            "Spanish"
-        )
-    },
-    {
-        "Name": "Western European",
-        "langs": (
-            "Dutcch",
-            "English",
-            "French",
-            "German",
-            "Italian",
-            "Norwegian",
-            "Portuguese",
-            "spanish"
-        )
-    },
-    {
-        "name": "Eastern European",
-        "langs": (
-            "English",
-            "Finnish",
-            "Polish",
-            "Romanian",
-            "Russian",
-            "Ukranian"
-        )
-    },
-    {
-        "name": "Middle Eastern/North African",
-        "langs": (
-            "Arabic",
-            "Burber",
-            "English",
-            "Farsi",
-            "French",
-            "Hebrew",
-            "Turkish"
-        )
-    },
-    {
-        "name": "Sub-Saharan African",
-        "langs": (
-            "Arabic",
-            "English",
-            "French",
-            "Hausa",
-            "Lingala",
-            "Oromo",
-            "Portugese",
-            "Swahihli",
-            "Twi",
-            "Yoruba"
-        )
-    },
-    {
-        "name": "South Asian",
-        "langs": (
-            "Bengali",
-            "Dari",
-            "English",
-            "Hindi",
-            "Nepali",
-            "Sinhalese",
-            "Tamil",
-            "Urdu"
-        )
-    },
-    {
-        "name": "South East Asian",
-        "langs": (
-            "Arabic",
-            "English",
-            "Filipeno",
-            "Hindi",
-            "Indonesian",
-            "Khmer",
-            "Malayan",
-            "Vietnamese"
-        )
-    },
-    {
-        "name": "East Asian",
-        "langs": (
-            "Cantonese",
-            "Chinese",
-            "English",
-            "Japanese",
-            "Korean",
-            "Mandarin Chinese",
-            "Mongolian"
-        )
-    },
-    {
-        "name": "Oceana/Pacific Islander",
-        "langs": (
-            "English",
-            "French",
-            "Hawaiian",
-            "Maori",
-            "Pama-Nyungan",
-            "Tahitan"
-        )
-    }
-]
+    "Affection you are never seen without?": (
+        "Tattoos",
+        "Mirrorshades",
+        "Ritual Scars",
+        "Spiked gloves",
+        "Nose rings",
+        "Tongue or other piercings",
+        "strange fingernail implants",
+        "Spiked boots or heels",
+        "fingerless gloves",
+        "Strange contacts"
+    ),
 
-# Your Personality
+    # Motivations and Relationships
 
-PERSONALITY = (
-    "Shy and secretive",
-    "Rebellious, antisocialm and violent",
-    "Arrogant, proud and aloof",
-    "Moody, rash and headstrong",
-    "Picky, fussy, nervous",
-    "Stable and serious",
-    "Silly and fluff-headed",
-    "Stable and serious",
-    "Sneaky and deceptive",
-    "intellectual and detached",
-    "Friendly and outgoing"
-)
+    "What do you value most?": (
+        "Money",
+        "Honor",
+        "Your word",
+        "Honesty",
+        "Knowledge",
+        "Vengance",
+        "Love",
+        "Power",
+        "Family",
+        "Friendship"
+    ),
+    "How do you feel about most people?": (
+        "I stay neutral.",
+        "I stay neutral.",
+        "I like almost everyone.",
+        "I like almost everyone.",
+        "People are tools.  Use them for your own good and discard them.",
+        "Every person is a valuable individual.",
+        "People are obstacles to be destroyed if they cross me.",
+        "People are Untrustworthy, don't depend on anyone.",
+        "Wipe'em out and let the cockroaches take over.",
+        "People are wonderful!"
+    ),
 
-# Dress and Personal Style
-
-CLOTHING = (
-    "Generic Chic (Standard, Colorful, Modular)",
-    "Leisurewear (Comfort, Agility, Athleticism",
-    "Urban Flash (Flashy, Technological, Streetwear)",
-    "Businesswear (Leadership, Presence, Authority)",
-    "High Fashion (Exclusive, Designer, Couture)",
-    "Bohemian (Folksy, Retro, Free-Spirited)",
-    "Bag Lady Chic (Homeless, Ragged, Vagrant)",
-    "Gang Colors (Dangerous, Violent, Rebelious)",
-    "Nomad Leathers (Western, Rugged, Tribal)",
-    "Asia Pop (Bright, Costume-like, Youthful)"
-)
-
-HAIRSTYLE = (
-    "Mohawk",
-    "Long and ratty",
-    "Short and spiked",
-    "Wild and all over",
-    "Bald",
-    "Striped",
-    "Wild colors",
-    "Neat and short",
-    "Short and curly",
-    "Long and straight"
-)
-
-ADORNMENT = (
-    "Tattoos",
-    "Mirrorshades",
-    "Ritual Scars",
-    "Spiked gloves",
-    "Nose rings",
-    "Tongue or other piercings",
-    "strange fingernail implants",
-    "Spiked boots or heels",
-    "fingerless gloves",
-    "Strange contacts"
-)
-
-# Motivations and Relationships
-
-MOTIVATIONS = (
-    "Money",
-    "Honor",
-    "Your word",
-    "Honesty",
-    "Knowledge",
-    "Vengance",
-    "Love",
-    "Power",
-    "Family",
-    "Friendship"
-)
-
-PEOPLE = (
-    "I stay neutral.",
-    "I stay neutral.",
-    "I like almost everyone.",
-    "I like almost everyone.",
-    "People are tools.  Use them for your own good and discard them.",
-    "Every person is a valuable individual.",
-    "People are obstacles to be destroyed if they cross me.",
-    "People are Untrustworthy, don't depend on anyone.",
-    "Wipe'em out and let the cockroaches take over.",
-    "People are wonderful!"
-)
-
-MOST_VALUED_PERSON = (
-    "A parent",
-    "A brother or sister",
-    "A lover",
-    "A friend",
-    "Yourself",
-    "A pet",
-    "A teacher or mentor",
-    "A public figure",
-    "A personal hero",
-    "No one"
-)
-
-MOST_VALUED_POSSESSION = (
-    "A weapon",
-    "A tool",
-    "A piece of clothing",
-    "A photograph",
-    "A book or diary",
-    "A recording",
-    "A musical instrument",
-    "A piece of jewelry",
-    "A toy",
-    "A letter"
-)
-
-# Your Background
-
-ORIGIN = (
-    "Corporate Execs",
-    "Corporate Managers",
-    "Corporate Technicians",
-    "Nomad Pack",
-    "Ganger 'Family'",
-    "Combat Zoners",
-    "Urban Homeless",
-    "Megastructure Warren Rats",
-    "Reclaimers",
-    "Edgerunners"
-)
-
-# Your Enviornment
-
-ENVIORNMENT = (
-    "Ran on the street with no adult supervision.",
-    "Spent a safe Corp Zone walled off from the rest of the City.",
-    "In a  Nomad pack moving from place to place",
-    "In a Nomad pack with roots in a transport (ships, planes, caravans).",
-    "In a decaying, once upscale neighborhood, now holding off the boosters to survive.",
-    "In the heart of the Combat Zone, living in a wrecked building or other squat.",
-    "In a huge 'megastructure' building controlled by a Corp of the City",
-    "In the ruins of a deserted city or twon taken over by Reclaimers.",
-    "In a Drift Nation (a floating offshore city) that is a meeting place for all kinds of people.",
-    "In a Corporate luxury 'starscraper', high above the rest of the teeming rabble."
-)
-
-# Family Crisis
-
-FAMILY_CRISIS = (
-    "Your family lost everything through betrayal.",
-    "Your family lost everything through bad management",
-    "Your family was exiled or otherwise driven from their orginal home/nation/Corporation.",
-    "Your family was imprisoned and you alone escaped.",
-    "Your family vanished. You are the only remaining member.",
-    "Your family was killed and you were the only survivor",
-    "Your family was involved with a long term conspiracy, organization, or assiociation, such as a crime family or\
-     revolutionary group."
-    "Your family was scattered to the winds due to misfortune.",
-    "Your family was cursed with a hereditary feud that has lasted for generations.",
-    "You are the inheritor of a family debt; You must honor this debt before moving on with your life."
-)
-
+    "Most valued person in your life?": (
+        "A parent",
+        "A brother or sister",
+        "A lover",
+        "A friend",
+        "Yourself",
+        "A pet",
+        "A teacher or mentor",
+        "A public figure",
+        "A personal hero",
+        "No one"
+    ),
+    "Most valued possession you own?": (
+        "A weapon",
+        "A tool",
+        "A piece of clothing",
+        "A photograph",
+        "A book or diary",
+        "A recording",
+        "A musical instrument",
+        "A piece of jewelry",
+        "A toy",
+        "A letter"
+    ),
+    "What's your original family origin?": (
+        "Corporate Execs",
+        "Corporate Managers",
+        "Corporate Technicians",
+        "Nomad Pack",
+        "Ganger 'Family'",
+        "Combat Zoners",
+        "Urban Homeless",
+        "Megastructure Warren Rats",
+        "Reclaimers",
+        "Edgerunners"
+    ),
+    "What was your childhood enviornment?": (
+        "Ran on the street with no adult supervision.",
+        "Spent a safe Corp Zone walled off from the rest of the City.",
+        "In a  Nomad pack moving from place to place",
+        "In a Nomad pack with roots in a transport (ships, planes, caravans).",
+        "In a decaying, once upscale neighborhood, now holding off the boosters to survive.",
+        "In the heart of the Combat Zone, living in a wrecked building or other squat.",
+        "In a huge 'megastructure' building controlled by a Corp of the City",
+        "In the ruins of a deserted city or twon taken over by Reclaimers.",
+        "In a Drift Nation (a floating offshore city) that is a meeting place for all kinds of people.",
+        "In a Corporate luxury 'starscraper', high above the rest of the teeming rabble."
+    ),
+    "What was your family crisis?": (
+        "Your family lost everything through betrayal.",
+        "Your family lost everything through bad management",
+        "Your family was exiled or otherwise driven from their orginal home/nation/Corporation.",
+        "Your family was imprisoned and you alone escaped.",
+        "Your family vanished. You are the only remaining member.",
+        "Your family was killed and you were the only survivor",
+        "Your family was involved with a long term conspiracy, organization, or assiociation, such as a crime family or\
+         revolutionary group."
+        "Your family was scattered to the winds due to misfortune.",
+        "Your family was cursed with a hereditary feud that has lasted for generations.",
+        "You are the inheritor of a family debt; You must honor this debt before moving on with your life."
+    )
+}
 # Your Friends
 
 FRIENDS = (
@@ -334,9 +315,9 @@ THROW_AT_YOU = (
     "Just themselves and even they won't go out of their way.",
     "Just themselves.",
     "Just themselves and a close friend.",
-    "Themselves and {} friends.".format(random.randint(1, 6) / 2),
-    "Thenselves and {} friends.".format(random.randint(1, 10) / 2),
-    "An entire gang of at least {} people.".format(random.randint(1, 10) + 5),
+    "Themselves and (1D6/2) friends.",
+    "Themselves and (1D10/2) friends.",
+    "An entire gang of at least (1D10 + 5) people.",
     "The local cops of other Lawmen.",
     "A powerful gang lord or small Corporation.",
     "A Powerful Corporation",
